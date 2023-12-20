9a6fe6f0c6280369c0a708c22c836f5e00f24aea
dagregi <120724828+dagregi@users.noreply.github.com>
2023-12-20 04:43:14 +0000

    scraper function

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 use std::path::PathBuf;
 
+use anyhow::Context;
 use askama::Template;
 use axum::{routing::get, Router};
+use scraper::{Html, Selector};
 use tower_http::services::ServeDir;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
-async fn main() {
+async fn main() -> anyhow::Result<()> {
     tracing_subscriber::registry()
         .with(
             tracing_subscriber::EnvFilter::try_from_default_env()
@@ -26,7 +28,8 @@ async fn main() {
 
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
     tracing::info!("listening on {}", addr);
-    axum::serve(listener, app).await.unwrap();
+
+    Ok(axum::serve(listener, app).await?)
 }
 
 async fn hello() -> HelloTemplate {
@@ -36,3 +39,33 @@ async fn hello() -> HelloTemplate {
 #[derive(Template)]
 #[template(path = "index.html")]
 struct HelloTemplate;
+
+struct ScrapedData {
+    title: String,
+    link: String
+}
+async fn scrape_download_links(url: &str) -> anyhow::Result<Vec<ScrapedData>> {
+    let body = reqwest::get(url).await?.text().await?;
+    // Parse the body
+    let document = Html::parse_document(&body);
+    let selector = Selector::parse(".sbutton").unwrap();
+    let mut scraped_data = Vec::new();
+    // Useing scraper to extract the link and title
+    for element in document.select(&selector) {
+        let link = element
+            .select(&Selector::parse("a").unwrap())
+            .next()
+            .and_then(|a| a.value().attr("href"))
+            .with_context(|| "Link not found")?;
+
+        let title = element
+            .select(&Selector::parse(".bottom").unwrap())
+            .next()
+            .map(|e| e.text().collect::<String>())
+            .with_context(|| "Couldn't parse the title")?;
+
+        scraped_data.push(ScrapedData {title, link: link.to_string()});
+    }
+
+    Ok(scraped_data)
+}
