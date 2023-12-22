c2ba5830c27f9e9d138c26872b177c3cc3bd86bc
dagregi <120724828+dagregi@users.noreply.github.com>
2023-12-22 08:21:33 +0000

    concurrency gotta love it

--- src/scrapers/download_page.rs
+++ src/scrapers/download_page.rs
@@ -1,7 +1,7 @@
 use anyhow::Context;
 use scraper::{Html, Selector};
 
-struct ScrapedData {
+pub struct ScrapedData {
     pub title: String,
     pub link: String,
 }
@@ -10,35 +10,33 @@ pub async fn download_page_scraper(url: &str) -> anyhow::Result<Vec<ScrapedData>
     // Parse the HTML
     let document = Html::parse_document(&body);
     let selector = Selector::parse(".sbutton").unwrap();
-
-    // Initialize an empty vector to store the scraped data
-    let mut scraped_data = Vec::new();
-    // Useing scraper to extract the link and title
-    for element in document.select(&selector) {
-        let a_tag = element
-            .select(&Selector::parse("a").unwrap())
-            .next()
-            .unwrap();
-        let link = a_tag
-            .value()
-            .attr("href")
-            .with_context(|| "Link not found")?;
-        let song_type = a_tag.text().collect::<String>();
-
-        let mut title = element
-            .select(&Selector::parse(".bottom").unwrap())
-            .next()
-            .map(|e| e.text().collect::<String>())
-            .with_context(|| "Couldn't parse the title")?;
-        if song_type.contains("Full version") {
-            title.push_str(" - (Full version)");
-        }
-
-        scraped_data.push(ScrapedData {
-            title,
-            link: link.to_string(),
-        });
-    }
+    // Extract the data and collect it
+    let scraped_data = document
+        .select(&selector)
+        .map(|element| {
+            let tag = element
+                .select(&Selector::parse("a").unwrap())
+                .next()
+                .unwrap();
+            let link = tag
+                .value()
+                .attr("href")
+                .with_context(|| "Link not found")?
+                .to_string();
+            let song_type = tag.text().collect::<String>();
+            let title = element
+                .select(&Selector::parse(".bottom").unwrap())
+                .next()
+                .map(|e| e.text().collect::<String>())
+                .with_context(|| "Couldn't parse the title")?;
+            let title = if song_type.contains("Full version") {
+                format!("{} - (Full version)", title)
+            } else {
+                title
+            };
+            Ok(ScrapedData { title, link })
+        })
+        .collect::<anyhow::Result<Vec<ScrapedData>>>()?;
 
     Ok(scraped_data)
 }
